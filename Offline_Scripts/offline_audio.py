d0ec6a579044bb86e651aa9e9ddc0b9bb4d073de
Shiva Khatri <shiva.khatri0001@gmail.com>
2024-07-01 18:52:29 -0400

    Successfully Added Local Ai to the local streamlit app.

--- Offline_Scripts/offline_audio.py
+++ Offline_Scripts/offline_audio.py
@@ -1,8 +1,8 @@
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 
-model_path = 'models/models--openai--whisper-medium/snapshots/abdf7c39ab9d0397620ccaea8974cc764cd0953e'
-
+model_path = '../models/models--openai--whisper-medium/snapshots/abdf7c39ab9d0397620ccaea8974cc764cd0953e'
+audio_path = "./audioDir/harvard.wav"
 pipe = pipeline("automatic-speech-recognition", model=model_path)
-
-out = pipe()
+out = pipe(audio_path)
+print(out)
--- Offline_Scripts/offline_module.py
+++ Offline_Scripts/offline_module.py
@@ -1,13 +1,58 @@
 from transformers import pipeline
 import torch
 import streamlit as st
+import os
 
 
+
+@st.cache_resource()
 def load_model_audio(model_path):
     model = pipeline("automatic-speech-recognition", model=model_path)
     return model
 
+import os
+
 def save_uploaded_audio(uploaded_file):
-    with open(uploaded_file.name, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    return uploaded_file.name
+    try:
+        # Create a directory to save the file
+        os.makedirs("audioDir", exist_ok=True)
+        
+        # Create the full file path
+        file_path = os.path.join("audioDir", uploaded_file.name)
+        
+        # Save the file to the directory
+        with open(file_path, "wb") as f:
+            f.write(uploaded_file.getbuffer())
+        
+        return file_path
+    except Exception as e:
+        return str(e)
+
+    
+def generate_text_from_audio(model, audio_file):
+    try:
+        file_path = save_uploaded_audio(audio_file)
+        result = model(file_path)
+        print(result)
+        return result
+    except Exception as e:
+        return str(e)
+    
+def main():
+    st.title("Audio to Text")
+    st.write("This is a simple audio to text conversion app using Hugging Face's pipeline API.")
+    model_path = "../models/models--openai--whisper-medium/snapshots/abdf7c39ab9d0397620ccaea8974cc764cd0953e"
+    model = load_model_audio(model_path)
+    
+    uploaded_file = st.file_uploader("Upload an audio file", type=["wav", "mp3", "ogg"])
+    
+    if uploaded_file:
+        if st.button("Transcribe"):
+            st.audio(uploaded_file, format="audio/wav")
+            with st.spinner('Transcribing...'):
+                result = generate_text_from_audio(model, uploaded_file)
+                st.write(result)
+       
+if __name__ == "__main__":
+    main()
+
