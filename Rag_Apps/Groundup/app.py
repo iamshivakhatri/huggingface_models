2a9a5a002c2ed1fb234b79ec4c0a84748b52ad12
Shiva Khatri <shiva.khatri0001@gmail.com>
2024-07-12 23:56:14 -0400

    Completed the streamlit app with dropdown to select the model.

--- Rag_Apps/Groundup/app.py
+++ Rag_Apps/Groundup/app.py
@@ -3,3 +3,6 @@ curl http://localhost:11434/api/generate -d '{
 "prompt": "How was world formed?",
 "stream": false
 }' | jq .
+
+
+ollama.chat(model='llama3', messages=[{'role': 'user', 'content': 'How can I start a startup?'}])
---
+++ Streamlit_apps/SimpleChatApp/chat_app.py
@@ -0,0 +1,33 @@
+import ollama
+import streamlit as st
+
+st.title("Simple Chat App")
+
+# initialize history
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+# Init models
+if "model" not in st.session_state:
+    st.session_state["model"] = ""
+
+models = [model["name"] for model in ollama.list()["models"]]
+st.session_state["model"] = st.selectbox("Select model", models)
+
+# Render chat messages from history on app
+for message in st.session_state["messages"]:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Whats up?"):
+    # Add latest message to history in format {role, content}
+    st.session_state["messages"].append({"role": "user", "content": prompt})
+
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        response = ollama.chat(model=st.session_state["model"], messages=st.session_state["messages"])
+        message = response["message"]["content"]
+        st.markdown(message)
+        st.session_state["messages"].append({"role": "assistant", "content": message})
