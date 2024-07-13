e0cc0afc129156fd832b42eb8a6ca2deb6e77f37
Shiva Khatri <shiva.khatri0001@gmail.com>
2024-07-13 00:05:56 -0400

    Completed the app with stream

--- Rag_Apps/Groundup/app.py
+++
@@ -1,8 +0,0 @@
-curl http://localhost:11434/api/generate -d '{ 
-"model": "phi3",
-"prompt": "How was world formed?",
-"stream": false
-}' | jq .
-
-
-ollama.chat(model='llama3', messages=[{'role': 'user', 'content': 'How can I start a startup?'}])
--- Streamlit_apps/SimpleChatApp/chat_app.py
+++ Streamlit_apps/SimpleChatApp/chat_app.py
@@ -14,6 +14,12 @@ if "model" not in st.session_state:
 models = [model["name"] for model in ollama.list()["models"]]
 st.session_state["model"] = st.selectbox("Select model", models)
 
+def model_res_generator():
+    stream = ollama.chat(model=st.session_state["model"], messages=st.session_state["messages"], stream=True)
+    for chunk in stream:
+        yield chunk["message"]["content"]
+
+
 # Render chat messages from history on app
 for message in st.session_state["messages"]:
     with st.chat_message(message["role"]):
@@ -27,7 +33,8 @@ if prompt := st.chat_input("Whats up?"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        response = ollama.chat(model=st.session_state["model"], messages=st.session_state["messages"])
-        message = response["message"]["content"]
-        st.markdown(message)
+        # response = ollama.chat(model=st.session_state["model"], messages=st.session_state["messages"])
+        # message = response["message"]["content"]
+        message = st.write_stream(model_res_generator())
+        # st.markdown(message)
         st.session_state["messages"].append({"role": "assistant", "content": message})
