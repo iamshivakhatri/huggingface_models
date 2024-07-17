5e008e73e76ece64f7f36c917b7760e672fb6a40
Shiva Khatri <shiva.khatri0001@gmail.com>
2024-07-16 23:19:16 -0400

    Working on diff version of langchain

--- Rag_Apps/app_with_langchain/app.py
+++ Rag_Apps/app_with_langchain/app.py
@@ -1,4 +1,19 @@
+#pip install "unstructured[all-docs]"
+
+
 from langchain_community.document_loaders import UnstructuredPDFLoader
 from langchain_community.document_loaders import OnlinePDFLoader
+
+
+local_path = "./story.pdf"
+
+# Load the PDF document from a local file
+if local_path:
+    pdf_loader = UnstructuredPDFLoader(local_path)
+    data = pdf_loader.load()
+else:
+    print("Please provide a valid path to a local PDF file.")
+
+print(data[0].page_content)
 
 
