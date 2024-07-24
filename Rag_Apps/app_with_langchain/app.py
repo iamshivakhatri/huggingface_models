6b73f390e4e987aca5352406377405aa5f46118c
Shiva Khatri <shiva.khatri0001@gmail.com>
2024-07-23 23:44:36 -0400

    Added few lines of code to rag application.

--- Rag_Apps/app_with_langchain/app.py
+++ Rag_Apps/app_with_langchain/app.py
@@ -4,6 +4,13 @@
 from langchain_community.document_loaders import UnstructuredPDFLoader
 from langchain_community.document_loaders import OnlinePDFLoader
 
+# Import for vector database and creating embeddings
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+
+
+
 
 local_path = "./story.pdf"
 
@@ -14,6 +21,19 @@ if local_path:
 else:
     print("Please provide a valid path to a local PDF file.")
 
-print(data[0].page_content)
+# print(data[0].page_content)
 
+# Split and chunk the text into smaller pieces
+splitter = RecursiveCharacterTextSplitter(chunk_size=7500, overlap_size=500)
+chunks = splitter.split_documents(data)
+
+# Add to the vector database
+vector_db = Chroma.from_documents(
+    documents=chunks,
+    embeddings=OllamaEmbeddings(model="nomic-embed-text", show_progress=True),
+    collection_name="local-rag"
+)
+
+
+   
 
